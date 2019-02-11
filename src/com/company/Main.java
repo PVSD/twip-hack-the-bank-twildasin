1b0b4875bb92c5cc6bab13edcf2e533a55bd84dd
tw073 <tw073@pvsd.org>
2019-02-11 12:06:08 -0500

    Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,22 +5,33 @@ import java.util.*;
 import java.text.*;
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException
+    {
         NumberFormat fmt = NumberFormat.getNumberInstance();
         fmt.setMinimumFractionDigits(2);
         fmt.setMaximumFractionDigits(2);
         String name;
+        FileWriter fw = new FileWriter("accounts.txt"); //page 142 in Blue Pelican
+        PrintWriter pw = new PrintWriter(fw);
+
+        FileWriter fwt = new FileWriter("transactions.txt");
+        PrintWriter trans = new PrintWriter(fwt);
+
+
+
         ArrayList aryLst = new ArrayList();
         ListIterator iter = aryLst.listIterator();
         do {
             Scanner kbReader = new Scanner(System.in);
             System.out
-                    .print("Please enter the name to whom the account belongs. (\"Exit\" to abort) ");
+                    .println("Please enter the name to whom the account belongs. (\"Exit\" to abort) ");
             name = kbReader.nextLine();
-            if (!name.equalsIgnoreCase("EXIT")) {
-                System.out.print("Please enter the amount of the deposit. ");
+            if (!name.equalsIgnoreCase("EXIT"))
+            {
+                pw.print("\n" + name);
+                System.out.println("Please enter the amount of the deposit. ");
                 double amount = kbReader.nextDouble();
+                pw.print(" : " + amount);
                 System.out.println(" "); // gives an eye pleasing blank line
                 // between accounts
                 bankAccount theAccount = new bankAccount(name, amount);
@@ -33,9 +44,11 @@ public class Main {
         bankAccount ba = (bankAccount) iter.previous();
         double maxBalance = ba.balance; // set last account as the winner so far
         String maxName = ba.name;
-        while (iter.hasPrevious()) {
+        while (iter.hasPrevious())
+        {
             ba = (bankAccount) iter.previous();
-            if (ba.balance > maxBalance) {
+            if (ba.balance > maxBalance)
+            {
                 // We have a new winner, chicken dinner
                 maxBalance = ba.balance;
                 maxName = ba.name;
@@ -45,6 +58,12 @@ public class Main {
         System.out.println("The account with the largest balance belongs to "
                 + maxName + ".");
         System.out.println("The amount is $" + fmt.format(maxBalance) + ".");
+
+
+        fw.close();
+        pw.close();
+        fwt.close();
+        trans.close();
 
     }
 }
